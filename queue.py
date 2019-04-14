1df14f6995bfda665cb5d7b0dfcd45cc6f051fe6
Shafayet Sadi <43206214+ShafayetSadi@users.noreply.github.com>
2019-04-14 16:37:27 +0600

    Added some comments

--- queue.py
+++ queue.py
@@ -1,21 +1,22 @@
 class Queue:
+	"""This class creates a stack data structure.It's main rule is FIFO(first in and first out)"""
 	def __init__(self):
 		self.items = []
 
 	def is_empty(self):
 		if self.items:
-			return False
+			return False #The stack is not empty.
 		else:
-			return True
+			return True #The stack is empty.
 
 	def enqueue(self, item):
-		return self.items.append(item)
+		return self.items.append(item) #Insert a item in the end.
 
 	def dequeue(self):
 		if is_empty():
 			print("Sorry!The queue is empty")
 		else:
-			return self.items.pop(0)
+			return self.items.pop(0) #Removes the first item.
 
 	def size(self):
 		return len(self.items)
