e5a24f9f3739eef8a9a343857d18b74ddc9af323
Shafayet Sadi <43206214+ShafayetSadi@users.noreply.github.com>
2019-04-17 10:57:44 +0600

    Bug fixed

--- queue.py
+++ queue.py
@@ -1,25 +1,25 @@
 class Queue:
-	"""This class creates a stack data structure.It's main rule is FIFO(first in and first out)"""
-	def __init__(self):
-		self.items = []
+    """This class creates a stack data structure.It's main rule is FIFO(first in and first out)"""
+    def __init__(self):
+        self.items = []
 
-	def is_empty(self):
-		if self.items:
-			return False #The stack is not empty.
-		else:
-			return True #The stack is empty.
+    def is_empty(self):
+        if self.items:
+            return False #The stack is not empty.
+        else:
+            return True #The stack is empty.
 
-	def enqueue(self, item):
-		return self.items.append(item) #Insert a item in the end.
+    def enqueue(self, item):
+        return self.items.append(item) #Insert a item in the end.
 
-	def dequeue(self):
-		if self.is_empty():
-			print("Sorry!The queue is empty")
-		else:
-			return self.items.pop(0) #Removes the first item.
+    def dequeue(self):
+        if self.is_empty():
+            print("Sorry!The queue is empty")
+        else:
+            return self.items.pop(0) #Removes the first item.
 
-	def size(self):
-		return len(self.items)
+    def size(self):
+        return len(self.items)
 
 
 my_queue = Queue()
--- stack.py
+++ stack.py
@@ -1,32 +1,32 @@
 class Stack:
-	"""This class creates a stack data structure.It's main rule is FILO(first in and last out)"""
-	def __init__(self):
-		self.stack = []
+    """This class creates a stack data structure.It's main rule is FILO(first in and last out)"""
+    def __init__(self):
+        self.stack = []
 
-	def is_empty(self):
-		if self.stack:
-			return False #The stack is not empty.
-		else:
-			return True #The stack is empty.
+    def is_empty(self):
+        if self.stack:
+            return False #The stack is not empty.
+        else:
+            return True #The stack is empty.
 
-	def push(self, item):
-		return self.stack.append(item) #Insert a item in the end.
+    def push(self, item):
+        return self.stack.append(item) #Insert a item in the end.
 
-	def pop(self):
-		if self.is_empty():
-			return 'Sorry!The stack is empty.'
-		else:
-			return self.stack.pop() #Removes the last item.
+    def pop(self):
+        if self.is_empty():
+            return 'Sorry!The stack is empty.'
+        else:
+            return self.stack.pop() #Removes the last item.
 
-	def size(self):
-		return len(self.stack)
+    def size(self):
+        return len(self.stack)
 
-	def peek(self):
-		return self.stack[len(self.stack)-1] #See the last item.
+    def peek(self):
+        return self.stack[len(self.stack)-1] #See the last item.
 
 if __name__ == '__main__':
-	my_stack = Stack()
-	print("The stack is empty:", my_stack.is_empty())
-	print('Adding Shafayet Sadi')
-	my_stack.push("Shafayet Sadi")
-	print("The stack is empty:", my_stack.is_empty())
+    my_stack = Stack()
+    print("The stack is empty:", my_stack.is_empty())
+    print('Adding Shafayet Sadi')
+    my_stack.push("Shafayet Sadi")
+    print("The stack is empty:", my_stack.is_empty())
